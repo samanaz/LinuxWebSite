1e11260616c756256fa31a9d7288e7fd8371bb2b
samanaz <saman.azadpour@hotmail.com>
2018-12-30 16:09:09 +1100

    Update About.cshtml.cs

--- Pages/About.cshtml.cs
+++ Pages/About.cshtml.cs
@@ -13,7 +13,7 @@ namespace simpleapp.Pages
         public void OnGet()
         {
             Message = "Your application description page.";
-            Message = "The second code change.";
+            Message = "The third code change.";
         }
     }
 }
